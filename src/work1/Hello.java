c28eac6c4694ccb59141b09bcbcbed6c25c48b7b
akiftasbas <32tasbasakif32@gmail.com>
2021-12-17 14:04:16 +0100

    Added more work

--- src/work1/Hello.java
+++ src/work1/Hello.java
@@ -5,6 +5,7 @@ public class Hello {
     public static void main(String[] args) {
         System.out.println("work1 started");
         System.out.println("created git repository");
+        System.out.println("Made a few commits");
 
     }
 }
